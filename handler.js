e3bd5dc148f6ef7ef777eca2dd675c0b45196b61
seandgrant <seandgrant@gmail.com>
2017-11-29 19:15:05 +1100

    oops 2

--- handler.js
+++ handler.js
@@ -1,7 +1,8 @@
-'use strict'
+'use strict';
 
 const awsServerlessExpress = require('aws-serverless-express');
 const app = require('./src/app');
 const server = awsServerlessExpress.createServer(app);
 
-export async main = (event, context) => awsServerlessExpress.proxy(server, event, context);
+exports.handler = (event, context) =>
+  awsServerlessExpress.proxy(server, event, context);
