cf56f8e348814e8686fcef1770d4a2614b4e0155
seandgrant <seandgrant@gmail.com>
2017-11-29 19:10:30 +1100

    oops

--- handler.js
+++ handler.js
@@ -4,4 +4,4 @@ const awsServerlessExpress = require('aws-serverless-express');
 const app = require('./src/app');
 const server = awsServerlessExpress.createServer(app);
 
-export async function main(event, context) => awsServerlessExpress.proxy(server, event, context);
+export async main = (event, context) => awsServerlessExpress.proxy(server, event, context);
