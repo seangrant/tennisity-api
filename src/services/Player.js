f7dbf555c64bb632083f93d58450149b112336f8
seandgrant <seandgrant@gmail.com>
2017-12-16 21:47:26 +1100

    wip

--- src/services/Player.js
+++ src/services/Player.js
@@ -3,7 +3,13 @@ import players from './Teamdata/Players';
 export const getPlayersForTeam = teamiId => {
   const result = players.filter(player => player.teamiId === teamiId);
   const teamPlayers = [0, 1, 2, 3].map(
-    id => result[id] || { id, teamiId, name: `Player ${id + 1}` }
+    id =>
+      result[id] || {
+        id,
+        teamiId,
+        name: `Player ${id + 1}`,
+        email: `player${id + 1}@example.org`
+      }
   );
   return teamPlayers;
 };
